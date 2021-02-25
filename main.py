119eb9b596c80d5f81823fc2e05717d572910ab2
Phoenix009 <jaideepmore307@gmail.com>
2021-02-26 00:35:53 +0530

    Better Solution

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 #TODO: What to do with the weight
 
@@ -10,20 +10,32 @@ for i in range(s):
     graph[a]['out'].add(name)
     graph[b]['in'].add(name)
 
+cars_count = defaultdict(int)
+for i in range(v):
+    n, *roads = input().split()
+    for road in roads: cars_count[road] += 1
+
 
 # NAIVE Solution
 ans = []
 for node in graph:
     if graph[node]['in']:
-        ans.append({
+        temp = {
             'id': node,
-            'count': len(graph[node]['in']),
-            #'duration': d//len(graph[node]['in'])
-            'duration': 1
-        })
+        }
+        ct = 0
+        road = []
+        for r_ in graph[node]['in']:
+            if cars_count[r_]:
+                ct += 1
+                road.append((r_, cars_count[r_]))
+        temp['count'] = ct
+        temp['roads'] = road
+        if len(road): ans.append(temp)
+
 
 print(len(ans))
 for i in ans:
     print(i['id'])
     print(i['count'])
-    for j in graph[i['id']]['in']: print(j, i['duration'])
+    for road, dur in i['roads']: print(road, dur)
