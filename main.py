5f52361978dc388a72de28b668bafb969b3861eb
Phoenix009 <jaideepmore307@gmail.com>
2021-02-26 01:43:21 +0530

    Vedants_Approach

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from collections import defaultdict, Counter
 
 #TODO: What to do with the weight
+def formula(wt, mini_wt, ct, mini_ct):
+    from math import ceil
+    return max(ceil((mini_wt/wt)*(ct/mini_ct)), 1)
 
 
 graph = defaultdict(lambda: {'in': set(), 'out':set()})
@@ -12,7 +15,7 @@ for i in range(s):
     a, b, name, wt = input().split()
     graph[a]['out'].add(name)
     graph[b]['in'].add(name)
-    weight[name] = wt
+    weight[name] = int(wt)
 
 
 cars_count = defaultdict(int)
@@ -20,13 +23,9 @@ for i in range(v):
     n, *roads = input().split()
     path_duration = 0
     for road in roads: 
-        path_duration += weight[road]
+        path_duration += int(weight[road])
     if path_duration <= d:
         for road in roads: cars_count[road] += 1
-
-
-
-
 
 
 # NAIVE Solution
@@ -38,14 +37,20 @@ for node in graph:
         }
         ct = 0
         road = []
-        mini = 10**9
+        mini_cars = 10**9
+        mini_wt = 10**9
         for r_ in graph[node]['in']:
             if cars_count[r_]:
                 ct += 1
-                mini = min(mini, cars_count[r_])
+                mini_cars = min(mini_cars, cars_count[r_])
+                mini_wt = min(mini_wt, weight[r_])
                 road.append((r_, cars_count[r_]))
         temp['count'] = ct
-        temp['roads'] = list(map(lambda x: (x[0], x[1]//mini), road))
+        roads_processed = []
+        for name, car_count in road:
+            roads_processed.append((name, formula(weight[name], mini_wt, cars_count[name], mini_cars)))
+        temp['roads'] = roads_processed
+
         if len(road): ans.append(temp)
 
 
